fa15f9bc4a91e2210df3278b6e8dc5c7122156d7
Vishw Modi <vishwmodi@gmail.com>
2026-01-21 00:59:59 +0530

    done changed the model to 2.5-flash

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: NextRequest) {
    console.log("recieved inputs:" ,fullPrompt);
    
 
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-001", });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash", });
 
     const formattedHistory = messages.map((msg: { from: string; text: string }) => ({
       role: msg.from === 'user' ? 'user' : 'model',
